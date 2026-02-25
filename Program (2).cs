30c657e6e3245f44c6625f0b5ecb6124e62c2926
Bu1o4ka <146112434+Bu1o4ka@users.noreply.github.com>
2026-02-25 20:12:10 +0300

    Add files via upload

---
+++ Program (2).cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 3, 30, 34, 5, 9 };
+            string result = Biggest(list);
+            Console.WriteLine(result);
+        }
+
+        static string Biggest(List<int> list)
+        {
+            List<string> listStr = new List<string>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                listStr.Add(list[i].ToString());
+            }
+            for (int i = 0; i < listStr.Count - 1; i++)
+            {
+                for (int j = 0; j < listStr.Count - 1 - i; j++)
+                {
+                    if (Bigger(listStr[j], listStr[j + 1]) == false)
+                    {
+                        string temp = listStr[j];
+                        listStr[j] = listStr[j + 1];
+                        listStr[j + 1] = temp;
+                    }
+                }
+            }
+
+            string u = "";
+            for (int i = 0; i < listStr.Count; i++)
+            {
+                u+= listStr[i].ToString();
+            }
+            return u;
+        }
+        
+         static bool Bigger(string a, string b)
+        {
+            string A = a + b;
+            string B = b + a;
+            if (A.Length == B.Length)
+            {
+                return string.Compare(A, B) > 0;
+            }
+            for (int i = 0; i < Math.Min(A.Length, B.Length); i++)
+            {
+                if (A[i] > B[i])
+                    return true;
+                if (A[i] < B[i])
+                    return false;
+            }
+
+            return A.Length > B.Length;
+        }
+    }
+}
---
+++ Program (3).cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworkeeeee
+{
+    internal class Program
+    {
+        static int FindMiss(int[] a)
+        {
+            int n = a.Length;
+            int expected = n * (n + 1) / 2;
+            int dontExpected = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                dontExpected += a[i];
+            }
+
+            return expected - dontExpected;
+        }
+        static string FindDuplicate(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        return $"Значение: {arr[i]}, индексы: {i} и {j}";
+                    }
+                }
+            }
+            return "Нет повторок";
+        }
+
+        static void Main()
+        {
+            int[] arr =  { 3, 0, 1, 4, 6, 2, 7, 8 };
+            Console.WriteLine($"Пропущенное число: {FindMiss(arr)}");
+
+            int[] arr2 = { 1, 4, 6, 6, 9, 12, 15 };
+            Console.WriteLine($"Дубль: {FindDuplicate(arr2)}");
+        }
+    }
+}
